88aef9c74d662ff1df99f99d0a9cab5aa750001a
jperez8089 <31641930+jperez8089@users.noreply.github.com>
2025-10-07 19:50:01 -0500

    knock knock

---
+++ knockjoke.py
@@ -0,0 +1,6 @@
+#The programs purpose is to demonstrate understanding of python's Print function
+print("Knock, Knock.")
+print("Who is there?")
+print("Radio")
+print("Radio Who?")
+print("Radio, not, here I come!")
