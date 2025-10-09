aab07b8d360f01dfb886bc2cb845f94af2fbaebf
jperez8089 <31641930+jperez8089@users.noreply.github.com>
2025-10-09 09:51:47 -0500

    Assignment to demonstarte knowledge of data types

---
+++ data.py
@@ -0,0 +1,21 @@
+# A program that demonstartes the understanding of data types
+# This is a Python program to demonstrate different data types
+
+# Literal
+print("Literal:", 13)
+
+# Integer
+y = 30
+print("Integer:", y)
+
+# Float
+z = 20.2
+print("Float:", z)
+
+# String
+name = "Javier"
+print("String:", name)
+
+# Boolean
+is_active = False
+print("Boolean:", is_active)
