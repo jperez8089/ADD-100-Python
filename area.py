aba83ea4f5cad06ff0ee7c442ea2576d59d705a5
jperez8089 <jperez8089@students.mchenry.edu>
2025-10-16 09:52:08 -0500

    a program of variables and calculations to calculate the area of a shape

---
+++ area.py
@@ -0,0 +1,12 @@
+'''The point of this program is to demonstrate the ability to make variables and using operations
+ by performing a simple calculation'''
+
+# declaring an variable for length
+length = 3
+# declaring an variable for width
+width = 6
+# Caluclating the area using the formula by involving the 2 variables  - the area will be a variable itself holding lxw 
+area = length * width
+
+#printing the area formula 
+print("The area of a rectangle with a length of:", length, "and a width of: ", width, "is: ", area)
