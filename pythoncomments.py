ec2c43404e07d513e4744920bb1db6d2f6856764
jperez8089 <jperez8089@students.mchenry.edu>
2025-10-22 12:58:31 -0500

    demonstarting four different comments

---
+++ pythoncomments.py
@@ -0,0 +1,12 @@
+'''Comment addition no.1 - Python Program to determine the Miles per gallon '''
+
+# Comment addition No.2 Variable for the size of the tank
+tank_size = 15
+# Define the total distance covered
+distance = 600
+# Calculate miles per gallon
+mpg = distance / tank_size # comment. no.3 - determining mpg 
+# Print the result
+print("Miles per gallon:", mpg)
+
+# TODO: Comment No.4 Allow user input for entering the tank size and distance
